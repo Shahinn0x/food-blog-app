c023022984d65382eb75873d49a55efef93d5c36
shahin <shahin14623@gmail.com>
2025-07-27 16:14:32 +0530

    feat: integrate frontend with backend API for recipe data

---
+++ frontend/food-blog-app/src/components/RecipeItems.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useLoaderData } from 'react-router-dom'
+import foodImg from '../assets/Biryani.jpg'
+import { FaStopwatch } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa6";
+
+export default function RecipeItems() {
+    const allRecipes=useLoaderData()
+    console.log(allRecipes)
+  return (
+    <>
+     <div className="card-container">
+        {
+            allRecipes?.map((Item,index)=>{
+                return (
+                    <div key={index} className="card">
+                        <img src={foodImg} width="120px" height="100px"></img>
+                        <div className="card-body">
+                            <div className="title">{Item.title}</div>
+                            <div className="icons">
+                                <div className="timer">
+                                    <FaStopwatch />30min
+                                </div>
+                                <FaHeart />
+                            </div>
+                        </div>
+                    </div>
+                )
+            })
+        }
+     </div> 
+    </>
+  )
+}
--- frontend/food-blog-app/src/pages/Home.jsx
+++ frontend/food-blog-app/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import f2 from '../assets/f4.jpg';
+import RecipeItems from '../components/RecipeItems';
+
 
 
 export default function Home() {
   return (
     <>
+       
         <section className='home'>
             <div className="left">
                 <h1>Food Recipe</h1>
@@ -18,6 +21,10 @@ export default function Home() {
         <div className="bg">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#d4f6e8" fillOpacity="1" d="M0,32L30,53.3C60,75,120,117,180,128C240,139,300,117,360,133.3C420,149,480,203,540,213.3C600,224,660,192,720,160C780,128,840,96,900,85.3C960,75,1020,85,1080,101.3C1140,117,1200,139,1260,149.3C1320,160,1380,160,1410,160L1440,160L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
         </div>
+        <div className='recipe'>
+            <RecipeItems/>
+        </div>
+       
     </>
   )
 }
